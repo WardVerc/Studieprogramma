094852cc1dc1920a372f96321b19823ee03e7599
Ward Vercruyssen <r0435477@student.vives.be>
2020-10-08 15:13:53 +0200

    Trying to commit the gitignore file take 2

--- src/main/java/be/vives/ti/App.java
+++ src/main/java/be/vives/ti/App.java
@@ -12,5 +12,6 @@ public class App
         System.out.println("First push to remote repository. Is gelukt.");
         System.out.println("Ik heb precies 2 branches in plaats van 1?");
         System.out.println("Found and added the gitignore file.");
+        System.out.println("git ignore file was not committed, trying this now");
     }
 }
