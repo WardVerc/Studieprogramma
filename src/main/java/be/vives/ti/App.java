a62adbc30b4f843965a0969775863d06442060cb
Ward Vercruyssen <r0435477@student.vives.be>
2020-10-08 15:30:34 +0200

    one last commit via cmd/git

--- src/main/java/be/vives/ti/App.java
+++ src/main/java/be/vives/ti/App.java
@@ -11,7 +11,7 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("First push to remote repository. Is gelukt.");
         System.out.println("Ik heb precies 2 branches in plaats van 1?");
-        System.out.println("Found and added the gitignore file.");
-        System.out.println("git ignore file was not committed, trying this now");
+        System.out.println("Found and committed the gitignore file.");
+        System.out.println("One final push to check if I can do it via cmd/git");
     }
 }
