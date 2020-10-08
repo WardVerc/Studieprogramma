a4c2e00fc1e841770ed37556e3137adade373422
Ward Vercruyssen <r0435477@student.vives.be>
2020-10-08 14:29:22 +0200

    Eerste commit om te probere pushen naar remote repo

---
+++ src/main/java/be/vives/ti/App.java
@@ -0,0 +1,15 @@
+package be.vives.ti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("First push to remote repository");
+        
+    }
+}
