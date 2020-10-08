6c9b7ce2f07c180f685136b451646ed9b9bc48c9
Ward Vercruyssen <r0435477@student.vives.be>
2020-10-08 14:44:21 +0200

    App.java - main - Hoe krijg ik een gitignore file?

--- src/main/java/be/vives/ti/App.java
+++ src/main/java/be/vives/ti/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("First push to remote repository");
-        
+        System.out.println("First push to remote repository. Is gelukt.");
+        System.out.println("Hoe krijg ik een gitignore file?");
     }
 }
